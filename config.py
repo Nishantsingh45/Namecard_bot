b4c9909d78ab40109bf7fc41421963cdc1686d11
Nishant Singh <68099982+Nishantsingh45@users.noreply.github.com>
2025-11-10 18:14:39 +0530

    changes in config.py
    Replaced hardcoded Supabase credentials with environment variables.

--- config.py
+++ config.py
@@ -15,5 +15,5 @@ class Config:
     # OpenAI Configuration
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     OPENAI_MODEL = "gpt-4o"
-    SUPABASE_URL = 'https://yraugxqmjylmrafggnqm.supabase.co'
-    SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyYXVneHFtanlsbXJhZmdnbnFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM3NDA3MjksImV4cCI6MjA0OTMxNjcyOX0.8F4ijY0N_f0FS4bU0C9WrTrwKvgQJRIyMprfyFdOnEU'
+    SUPABASE_URL = os.getenv("SUPABASE_URL")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY")
